bfd194ee6127595b7024eec920ccd23ed9d2ccd0
Paketic <129423521+Paketic@users.noreply.github.com>
2023-03-30 20:27:28 +0300

    Add files via upload

---
+++ lab20.pas
@@ -0,0 +1,19 @@
+﻿var i,j,n: longint;
+begin
+read(n);
+for i:=1 to n do
+  begin
+    for j:=1 to n do
+      write(i+j,' ');
+      writeln;
+  end;
+writeln;
+writeln;
+for i:=1 to n do
+  begin
+    for j:=1 to n do
+      write(i*j,' ');
+      writeln;
+  end;
+ 
+End.
